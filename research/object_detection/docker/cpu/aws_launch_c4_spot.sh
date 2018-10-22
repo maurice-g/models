5802dd6e413f0d2673fdda38936af5ca496fbea4
Ubuntu <ubuntu@ip-172-31-21-20.eu-central-1.compute.internal>
2018-10-22 21:06:20 +0000

    (feature) update docker configuration with beefier instance

--- research/object_detection/docker/cpu/aws_launch_c4_spot.sh
+++ research/object_detection/docker/cpu/aws_launch_c4_spot.sh
@@ -7,11 +7,12 @@ AWS_VPC=vpc-993aeff0
 docker-machine create --driver amazonec2 \
                       --amazonec2-ami $AWS_AMI \
                       --amazonec2-region eu-central-1 \
-                      --amazonec2-instance-type c4.large \
+                      --amazonec2-instance-type c4.2xlarge \
                       --amazonec2-security-group default \
 		      --amazonec2-vpc-id $AWS_VPC \
                       --amazonec2-root-size 32 \
                       --amazonec2-request-spot-instance \
-                      --amazonec2-spot-price "0.5" \
+                      --amazonec2-spot-price "1.5" \
                       --amazonec2-zone a \
 		      $1
+
