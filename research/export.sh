86c2f2b1941e1105195a0e0cd733e711c7ba260a
pdeburen <pascal@caplena.com>
2018-12-12 10:38:24 +0000

    (fix) updated export script to work as train.sh and eval.sh

--- research/export.sh
+++ research/export.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 export PYTHONPATH=$PYTHONPATH:`pwd`:`pwd`/slim
-python object_detection/export_inference_graph.py --pipeline_config_path $1*.config --input_type encoded_image_string_tensor --trained_checkpoint_prefix $1$2 --output_directory $3
+objdetpath=$(pwd)
+cd $1 && python $objdetpath/object_detection/export_inference_graph.py --pipeline_config_path $1*.config --input_type encoded_image_string_tensor --trained_checkpoint_prefix $1/train/$2 --output_directory $3
