cc1ea3ffe6ff24009e7a33dbf0fc68563854a921
pdeburen <pascal@caplena.com>
2019-01-31 09:58:30 +0000

    (feature) updated train.sh and eval.sh to work with new version of API

--- research/eval.sh
+++ research/eval.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export PYTHONPATH=$PYTHONPATH:`pwd`:`pwd`/slim
 objdetpath=$(pwd)
-cd $1 && python $objdetpath/object_detection/eval.py --logtostderr --pipeline_config_path $1/train/*.config --checkpoint_dir $1/train --eval_dir $1/eval
+cd $1 && python $objdetpath/object_detection/legacy/eval.py --logtostderr --pipeline_config_path $1/train/*.config --checkpoint_dir $1/train --eval_dir $1/eval
--- research/train.sh
+++ research/train.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export PYTHONPATH=$PYTHONPATH:`pwd`:`pwd`/slim
 objdetpath=$(pwd)
-cd $1 && python $objdetpath/object_detection/train.py --logtostderr --pipeline_config_path $1*.config --train_dir $1/train
+cd $1 && python $objdetpath/object_detection/legacy/train.py --logtostderr --pipeline_config_path $1*.config --train_dir $1/train
